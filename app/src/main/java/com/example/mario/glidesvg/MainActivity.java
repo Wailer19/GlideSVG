8c3c83e602a7361d79e700ea32cbf609524a6c48
Mario Mejia <Rastone16@gmail.com>
2017-12-06 20:19:10 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mario/glidesvg/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.mario.glidesvg;
+
+import android.content.ContentResolver;
+import android.graphics.drawable.PictureDrawable;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.RequestBuilder;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.util.Preconditions;
+
+import java.io.File;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "SVGActivity";
+
+    private ImageView imageViewRes;
+    private ImageView imageViewNet;
+    private RequestBuilder<PictureDrawable> requestBuilder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageViewNet = (ImageView) findViewById(R.id.image);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        requestBuilder = GlideApp.with(MainActivity.this)
+                .as(PictureDrawable.class)
+                .placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .override(5,5)
+                .priority(Priority.HIGH)
+                .skipMemoryCache(true)
+                .listener(new SvgSoftwareLayerSetter(){
+                    @Override
+                    public boolean onLoadFailed(GlideException e, Object model, Target<PictureDrawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+
+                    }
+
+                    @Override
+                    public boolean onResourceReady(PictureDrawable resource, Object model, Target<PictureDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                });
+
+        loadNet();
+    }
+
+
+
+    private void loadNet() {
+        Uri uri = Uri.parse("http://179.51.65.174:8080/StateTrustWeb/RESTful/WsVehicles/GetDocumentMarbeteAutoBackSVG?5=do");
+        requestBuilder.load(uri).into(imageViewNet);
+
+        //http://179.51.65.174:8080/StateTrustWeb/RESTful/WsVehicles/GetDocumentMarbeteAutoSVG?1=1-05-406577&2=297749&3=2&4=1&5=DO&6=660477
+        //http://179.51.65.174:8080/StateTrustWeb/RESTful/WsVehicles/GetDocumentMarbeteAutoBackSVG?5=do
+    }
+}
---
+++ app/src/main/java/com/example/mario/glidesvg/SvgDrawableTranscoder.java
@@ -0,0 +1,24 @@
+package com.example.mario.glidesvg;
+
+import android.graphics.Picture;
+import android.graphics.drawable.PictureDrawable;
+
+import com.bumptech.glide.load.Options;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.resource.SimpleResource;
+import com.bumptech.glide.load.resource.transcode.ResourceTranscoder;
+import com.caverock.androidsvg.SVG;
+
+/**
+ * Created by mario on 27/11/17.
+ */
+
+public class SvgDrawableTranscoder implements ResourceTranscoder<SVG, PictureDrawable> {
+    @Override
+    public Resource<PictureDrawable> transcode(Resource<SVG> toTranscode, Options options) {
+        SVG svg = toTranscode.get();
+        Picture picture = svg.renderToPicture();
+        PictureDrawable drawable = new PictureDrawable(picture);
+        return new SimpleResource<>(drawable);
+    }
+}
